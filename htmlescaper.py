17aece761e96880d45cdbdb993e7a2105604da47
unknown <GuenterC@DEWSANIS186.dehgnt.hansgrohe.net>
2013-06-30 17:55:47 +0200

    Converter

--- htmlescaper.py
+++ htmlescaper.py
@@ -1,6 +1,7 @@
 import os
 import webapp2
 import jinja2
+import cgi
 
 from google.appengine.ext import db
 
@@ -10,16 +11,22 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-    
+        
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
     
     def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+        self.write(self.render_str(template, **kw))  
         
 class Main(Handler):
     def get(self):
-        self.write("Test")
+        self.render('test.html')
+        
+    def post(self):
+        params = {}
+        params['input'] = self.request.get("input")
+        params['output'] = cgi.escape(self.request.get("input"))
+        self.render('test.html',**params)
                     
 app = webapp2.WSGIApplication([('/',Main)])
