db78e418aeef79323ef2fe26126ea07dee2f0eef
masterr <chguenter@web.de>
2013-07-06 09:06:14 +0200

    JQuery-UI

--- static/jquery_scripts.js
+++ static/jquery_scripts.js
@@ -1,4 +1,15 @@
 
+
 $(document).ready(function() {
 	$('#formatted').append($('#raw').text());
+	
+});		
+$(function() {
+	$('button')
+		.button({disabled: false})
+		.height("30px")
+		.width("150px")
+		.click(function( event ) {
+			
+	});
 });
